b7fde21eb01bbde66047cf8e748268441a5908ff
Anurag Kumar Maurya <113245724+anurag8773@users.noreply.github.com>
2024-04-25 23:23:38 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+const apiUrl = "https://377c3ca6-406d-4e9d-b99b-ae0112c12374-dev.e1-us-east-azure.choreoapis.dev/djangoreact/backend/rest-api-be2/v1.0";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
